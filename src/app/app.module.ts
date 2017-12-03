0e054d59b57ede0e6dcce66a446377ced5447ff1
aswin567 <toaswink@gmail.com>
2017-12-04 00:03:26 +0530

    Service integrated, map functionality started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import {MatTableModule, MatPaginatorModule, MatSortModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import {MatTableModule, MatPaginatorModule, MatSortModule, MatFormFieldModule, MatInputModule, MatDialogModule} from '@angular/material';
 import 'hammerjs';
+import { AgmCoreModule } from '@agm/core';
+import { MapComponent } from './map/map.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     BrowserAnimationsModule,
+    MatDialogModule,
     MatTableModule,
     MatFormFieldModule,
     MatInputModule,
     MatSortModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyC2QQeYf0sJgU95YhZUamJf4sBxnexO62E'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
